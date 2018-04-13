4cb0b25ffcd0b1079b3fbdc953c5fbd12542e0a9
smontoro <sarahjmontoro@gmail.com>
2018-04-12 21:49:48 -0700

    button functions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
 new Vue({
 	el: "#main",
 	data: {
-		playerHealth: 80,
-		monsterHealth: 80
+		isRunning: false,
+		playerHealth: 100,
+		monsterHealth: 100
+	},
+	methods: {
+		start: function() {
+			this.isRunning = true
+		},
+		giveUp: function() {
+			this.isRunning = false
+			this.playerHealth = 100
+			this.monsterHealth = 100
+		},
+		attack: function() {
+			this.playerHealth -= Math.floor(Math.random() * 18) + 6
+			this.monsterHealth -= Math.floor(Math.random() * 10) + 3
+		},
+		special: function() {
+			this.playerHealth -= Math.floor(Math.random()*18) + 6
+			this.monsterHealth -= Math.floor(Math.random() *13) +3
+		},
+		heal: function() {
+			this.playerHealth +=Math.floor(Math.random() * 16) + 10
+			this.playerHealth -=Math.floor(Math.random() * 18) + 6
+		}, 
 	}
 	
 })
