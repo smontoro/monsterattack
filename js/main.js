f814590b927fab41400147de07745bed18b0f108
smontoro <sarahjmontoro@gmail.com>
2018-04-19 21:45:49 -0700

    test

--- js/main.js
+++ js/main.js
@@ -1,31 +1,33 @@
-new Vue({
-	el: "#main",
-	data: {
-		isRunning: false,
-		playerHealth: 100,
-		monsterHealth: 100
+const one = {
+	template: '<h1>One</h1>'
+}
+const two = {
+	template: '<h1>Two</h1>'
+}
+const three = {
+	template: '<h1>Three</h1>'
+}
+
+const router = new VueRouter({
+	routes: [
+	{
+		path:'/one',
+		component: one
 	},
-	methods: {
-		start: function() {
-			this.isRunning = !this.isRunning
-			this.playerHealth = 100
-			this.monsterHealth = 100
-		},
-		calcDamage: function(max, min) {
-			return Math.floor(Math.random() * max) + min
-		},
-		attack: function() {
-			this.playerHealth -= calcDamage(18, 6)
-			this.monsterHealth -= calcDamage(10, 3)
-		},
-		special: function() {
-			this.playerHealth -= Math.floor(Math.random()*18) + 6
-			this.monsterHealth -= Math.floor(Math.random() *13) +3
-		},
-		heal: function() {
-			this.playerHealth +=Math.floor(Math.random() * 16) + 10
-			this.playerHealth -=Math.floor(Math.random() * 18) + 6
-		}, 
+		{
+		path:'/two',
+		component: two
+	},
+		{
+		path:'/three',
+		component: three
 	}
-	
+	]
 })
+
+var routeTest = new Vue ({
+	el: '#app',
+	data: {
+
+	},
+}).$mount('app')
